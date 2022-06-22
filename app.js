f62ea7a40607641bd0278e469f638d9fbc3500fb
TarekApps <mizzy887x@gmail.com>
2022-06-22 16:30:51 +0100

    added score and win conditions

--- app.js
+++ app.js
@@ -3,23 +3,36 @@ const roll = document.getElementById("roll");
 const dice = document.getElementById("dice");
 
 let score = 0;
-scoreNum.innerText = score;
+
 roll.addEventListener("click", () => {
   rollDice = Math.floor(Math.random() * 6) + 1;
   if (rollDice == 1) {
     dice.src = "/images/dice1.png";
-    score += 1;
+    scoreNum.innerText = "YOU LOSE";
+    score = 0;
   } else if (rollDice == 2) {
     dice.src = "/images/dice2.png";
     score += 2;
+    scoreNum.innerText = score;
   } else if (rollDice == 3) {
     dice.src = "/images/dice3.png";
     score += 3;
+    scoreNum.innerText = score;
   } else if (rollDice == 4) {
     dice.src = "/images/dice4.png";
+    score += 4;
+    scoreNum.innerText = score;
   } else if (rollDice == 5) {
     dice.src = "/images/dice5.png";
+    score += 5;
+    scoreNum.innerText = score;
   } else if (rollDice == 6) {
     dice.src = "/images/dice6.png";
+    score += 6;
+    scoreNum.innerText = score;
+  }
+  if (score >= 20) {
+    scoreNum.innerText = "YOU WIN!";
+    score = 0;
   }
 });
