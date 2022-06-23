21353d093168ef32f4fdfeadb388a6d7973a2865
TarekApps <mizzy887x@gmail.com>
2022-06-23 11:33:40 +0100

    added winning message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const scoreNum = document.getElementById("scoreNum");
 const roll = document.getElementById("roll");
 const dice = document.getElementById("dice");
+const scoreText = document.getElementById("scoreText");
 
 let score = 0;
 
@@ -14,25 +15,31 @@ roll.addEventListener("click", () => {
     dice.src = "/images/dice2.png";
     score += 2;
     scoreNum.innerText = score;
+    scoreText.innerText = "Score";
   } else if (rollDice == 3) {
     dice.src = "/images/dice3.png";
     score += 3;
     scoreNum.innerText = score;
+    scoreText.innerText = "Score";
   } else if (rollDice == 4) {
     dice.src = "/images/dice4.png";
     score += 4;
     scoreNum.innerText = score;
+    scoreText.innerText = "Score";
   } else if (rollDice == 5) {
     dice.src = "/images/dice5.png";
     score += 5;
     scoreNum.innerText = score;
+    scoreText.innerText = "Score";
   } else if (rollDice == 6) {
     dice.src = "/images/dice6.png";
     score += 6;
     scoreNum.innerText = score;
+    scoreText.innerText = "Score";
   }
   if (score >= 20) {
-    scoreNum.innerText = "YOU WIN!";
+    scoreNum.innerText = score;
+    scoreText.innerText = "YOU WIN!";
     score = 0;
   }
 });
